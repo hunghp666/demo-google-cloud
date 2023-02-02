c4a739bfda461e9f1bccc7cbbc50581606251631
hunghp666 <hung.hp666@gmail.com>
2023-02-02 09:39:59 +0700

    commit

--- src/main/java/com/archtech/demogooglecloud/TestController.java
+++ src/main/java/com/archtech/demogooglecloud/TestController.java
@@ -21,7 +21,7 @@ public class TestController {
 
     @GetMapping ("/hello")
     public String hello(){
-        return "Hello from Google Cloud";
+        return "Hello from Google Cloud, Trigger by GithubAction!";
     }
 
     @PostMapping("/books")
